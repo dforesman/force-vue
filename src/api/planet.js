168b18c950350d6097222b7201853fbccb1865ad
David Foresman <foresman@gmail.com>
2016-10-25 18:23:35 -0400

    ports random image retrieval from Person to Planet view - implements quick-and-dirty POC of portable image search code. moving to generalize

--- src/api/planet.js
+++ src/api/planet.js
@@ -5,6 +5,7 @@ import { Promise } from 'es6-promise';
 import { ApiCache } from './api-cache.js';
 
 const planetCache = new ApiCache();
+const pictureCache = Object.create(null);
 const planet = new EventEmitter();
 const planetBaseUrl = 'planets/';
 export default planet;
@@ -23,6 +24,38 @@ planet.fetch = id => {
       const planetData = response.data;
       planetCache.put(planetToGet, planetData);
       resolve(planetData);
+    }, reject);
+  });
+};
+
+
+planet.getPicture = searchWord => {
+  if (!searchWord) {
+    return Promise.resolve('');
+  }
+  return new Promise((resolve, reject) => {
+    if (pictureCache[searchWord]) {
+      resolve(pictureCache[searchWord]);
+    }
+    const param = {
+      q: searchWord,
+      q_type: 'jpg',
+    };
+    Vue.http.headers.common.Authorization = '';
+    Vue.http.get(process.env.IMAGE_HOST, param, {
+      headers: {
+        Authorization: process.env.IMGUR_CLIENT_ID,
+      },
+    }).then(response => {
+      let imgUrl = null;
+      if (typeof response.data === 'undefined' || typeof response.data.data[0] === 'undefined') {
+        imgUrl = null;
+      } else if (typeof response.data.data[0].cover !== 'undefined') {
+        imgUrl = 'http://i.imgur.com/'.concat(response.data.data[0].cover, 'm.jpg');
+      } else if (typeof response.data.data[0].link !== 'undefined') {
+        imgUrl = response.data.data[0].link.replace(/\.jpg$/, 'm.jpg');
+      }
+      resolve(imgUrl);
     }, reject);
   });
 };
--- src/components/ViewPlanet.vue
+++ src/components/ViewPlanet.vue
@@ -13,6 +13,13 @@
     <mdl-textfield data-qa="surfaceWaterTextbox" floating-label="Surface Water" :value.sync="planetData.surface_water"></mdl-textfield>
     <mdl-textfield data-qa="populationTextbox" floating-label="Population" :value.sync="planetData.population"></mdl-textfield>
   </div>
+  <div class="mdl-cell mdl-card mdl-shadow--4dp">
+    <figure class="mdl-card__media">
+      <div v-show="loading" class="mdl-spinner mdl-spinner--single-color mdl-js-spinner is-active"></div>
+      <img v-show="imageShow" :src.sync="planetImage">
+      <span>{{planetImageMsg}}</span>
+    </figure>
+  </div>
 </template>
 
 <script>
@@ -38,15 +45,46 @@ export default {
         surface_water: '',
         population: '',
       },
+      planetImage: '',
+      planetImageMsg: '',
+      imageShow: false,
+      loading: false,
     };
   },
   methods: {
     fetchRandomPlanet() {
+      this.loading = true;
+      this.imageShow = false;
+      this.planetImageMsg = '';
+      this.planetImage = '';
       const randomPlanetId = Math.floor((Math.random() * 61) + 1);
       planet.fetch(randomPlanetId).then((planetData) => {
         this.planetData = Object.assign(this.planetData, planetData);
+        planet.getPicture(this.planetData.name).then((data) => {
+          this.loading = false;
+          if (data) {
+            this.planetImage = data;
+            this.imageShow = true;
+          } else {
+            this.planetImage = '';
+            this.planetImageMsg = 'No image found.';
+          }
+        });
       });
     },
   },
 };
 </script>
+
+
+<style scoped>
+h1 {
+  color: black;
+}
+figure img {
+  width: 285px;
+}
+.mdl-card__media {
+  background-color: #FFF;
+}
+</style>
